85988d09d108de8e5b4055d7a329d9a791f402a2
王蕾 <wangl@mti-sh.cn>
2023-09-08 18:07:37 +0800

    修改框架

---
+++ app/src/main/java/org/strong/wind/Application.java
@@ -0,0 +1,18 @@
+package org.strong.wind;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author 王蕾
+ * @创建时间 ${DATE} $TIME $DAY_NAME_SHORT
+ * @描述
+ */
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
